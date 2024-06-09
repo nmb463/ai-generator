dc023292e8045d8f97b9871b74560f188601a6c1
Nicole Banda <nmbanda124@gmail.com>
2024-06-09 12:02:27 -0700

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generatePoem(event) {
     let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a single <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem using a <strong> element with 'SheCodes AI' at the end of the poem. There is no need to state that it is html";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">Generating a poem for you about ${instructionsInput.value}...</div>`;
+
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context"${context}`);
